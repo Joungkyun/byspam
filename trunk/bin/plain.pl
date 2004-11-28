e5b6603e71b931a6d78b7cdff5a8ee0c10597d2f
oops <oops@459c35ce-65cd-11e0-86f3-b8ba8ad78e03>
2004-11-28 09:48:49 +0000

    rewrite plain.pl

--- trunk/bin/plain.pl
+++ trunk/bin/plain.pl
@@ -2,107 +2,159 @@
 #
 # Mail Parse Utility supported by BySPAM
 # JoungKyun Kim <http://www.oops.org>
-# $Id: plain.pl,v 1.2 2004-11-27 19:06:48 oops Exp $
+# $Id: plain.pl,v 1.3 2004-11-28 09:48:49 oops Exp $
 #
-use Mail::Internet;
-use Mail::Header;
-use MIME::Base64;
-use MIME::QuotedPrint;
+use lib '@includedir@';
+my $conf = "@confdir@/byspam.conf";
 
-local $includeDir;
+use strict;
 
-# get config file
-$conf = "@confdir@/byspam.conf";
-if ( -f "$conf" ) { do "$conf"; }
+use Byspam::Getopt;
+use Byspam::Common;
+use Byspam::Parse;
+use Byspam::Mail;
 
-# get sub function
-do "$includeDir/byspamFunction.pl";
+# declaration global variable on configuration file
+use vars qw ($version  $level $allows $ignore $filterIframe $filterTag);
+use vars qw ($nobody $noheader $charset @basics $trashPeriod);
+use vars qw ($binDir  $confdir $filterDir $includeDir $perlpath);
 
-if($#ARGV < 0) {
-  Help();
-} 
+my $cm;
+my $o;
+my $ov;
 
-my $paras;
-my @para = ();
-foreach (@ARGV) { $paras .= " ".$_; }
-if ($paras !~ /-(h|f)/) { Help(); }
-@para = split(/ -/,$paras);
+# options variable
+my $opt;
+my %_opt;
+my $_file;
 
-my $file;
-my $printBody;
-my $printHead;
-LINE: foreach (@para) {
-  if($_ !~ /^[\s]*$/i) {
-    if ($_ =~ /^-?h/) { Help(); }
-    if ($_ =~ /^B/) { $printBody = 1; next LINE; }
-    if ($_ =~ /^H/) { $printHead = 1; next LINE; }
-    if ($_ =~ /^f/) {
-      $file = $_;
-      $file =~ s/^f[\s]+//go;
-      if( ! -f $file ) { print "Error: $file not found!\n"; exit; }
-      next LINE;
-    }
-  }
+# get argument on shell
+my $_argc    = $#ARGV; 
+my @_argv    = @ARGV;
+
+# get configuration file
+if ( ! -f "$conf" ) {
+	print "\n";
+	print "    Configuration file missing.\n";
+	print "    Check \"$conf\" file\n";
+	print "\n";
+	exit 1;
 }
 
-if ($paras !~ /-(B|H)/) { $printBody = $printHead = 1; }
+require $conf;
 
+if ( ! $charset ) {
+	$charset = $ENV{"LANG"};
+	CHARSET: {
+		( $charset =~ m/^ko/i ) and $charset = "EUC-KR", last CHARSET;
+		( $charset =~ m/^utf/i ) and $charset = "UTF-8", last CHARSET;
+		$charset = "";
+	}
+}
+
+# create reference Common
+$cm = new Byspam::Common;
+
+# create reference Getopt
+$o = new Byspam::Getopt;
+$ov = $o->{_var};
+
+# match long option whit short option
+$ov->{longopt} = {
+	'body'        => 'B',
+	'header'      => 'H',
+	'help'        => 'h'
+};
+
+$_opt{body}   = 0;
+$_opt{header} = 0;
+
+while ( 1 ) {
+	$opt = $o->getopt ("BHvh", $_argc + 1, @_argv);
+	last if ( ! $opt );
+
+	SWITCH: {
+		( $opt eq "B" ) and do {
+			$_opt{body} = 1;
+			last SWITCH;
+		};
+		( $opt eq "H" ) and do {
+			$_opt{header} = 1;
+			last SWITCH;
+		};
+
+		Help ();
+	}
+}
+
+Help () if ( $ov->{getopt_err} or $ov->{optcno} != 1 );
+
+$_file = $ov->{optcmd}->[0];
+if ( ! $_opt{body} && ! $_opt{header} ) {
+	$_opt{body} = 1;
+	$_opt{header} = 1;
+}
+
+
+my $ps;
 my $mail;
 my @mailText = ();
-@mailText = getContext($file);
 
-$mail = Mail::Internet->new(\@mailText);
+@mailText = $cm->getContext_rr ($_file);
 
-if($printHead || $printBody) {
-  system "clear";
-  print "[ Result of Decoding $file ]\n\n";
+$mail = Byspam::Mail->new (\@mailText);
+$ps = Byspam::Parse->new ();
+
+system "clear";
+print "[ Result of Decoding $_file ]\n\n";
+
+if ( $_opt{header} ) {
+	print "####  Mail Header  #############################################\n";
+	print "\n";
+	print $ps->getHeader ($mail->{_header}) . "\n\n";
 }
 
-if($printHead) {
-  $header = getHeader($mail);
-  print "#####  Mail Header  #############################################\n";
-  print "\n";
-  print "$header\n\n";
+if ( $_opt{body} ) {
+	print "#####  Mail Body   #############################################\n";
+	print "\n";
+	print $ps->getBody ($mail) . "\n\n";
 }
 
-if($printBody) {
-  $body = getBody($mail);
-  print "#####   Mail Body   #############################################\n";
-  print "\n";
-  print "$body\n";
-}
 
 sub Help {
-  my $lc;
-  my $lcn; 
-  $lcn = $ENV{"LANG"};
-  if ($lcn) { $lcn =~ s/^ko.*/ko/igo; }
-  if ($lcn && $lcn eq "ko") { $lc = 1; }
+	my @helps = ();
+	my $USAGES;
 
-  my @helps = ();
-  my $USAGES;
-  if ($lc) {
-    $USAGES = "사용법";
-    @helps = (
-              "현재 메세지를 출력",
-              "지정한 메일의 헤더와 본문을 디코딩하여 출력",
-              "메일 본문을 디코딩 하여 출력",
-              "메일 헤더를 디코딩 하여 출력"
-             );
-  } else {
-    $USAGES = "USAGE";
-    @helps = (
-               "print this message",
-               "print out decoded header and body in filename",
-               "print out decoded mail body in filename",
-               "print out decoded mail header in filename"
-             );
-  }
+	if ( $main::charset eq "EUC-KR" ) {
+		$USAGES = "사용법";
+		@helps = (
+					"현재 메세지를 출력",
+					"지정된 파일의 메일 본문만 디코딩 하여 출력",
+					"지정된 파일의 메일 헤더만 디코딩 하여 출력"
+		);
+	} else {
+		$USAGES = "USAGE";
+		@helps = (
+					"print this message",
+					"print out decoded mail body in filename",
+					"print out decoded mail header in filename"
+		);
+	}
 
-  print "$USAGES : $0 -[hHB] -f filename\n";
-  print "  -h --help   => $helps[0]\n";
-  print "  -f filename => $helps[1]\n";
-  print "  -B          => $helps[2]\n";
-  print "  -H          => $helps[3]\n";
-  exit 1;
+	print "$USAGES : $0 -[hHB] filename\n";
+	print "  -h --help   => $helps[0]\n";
+	print "  -B --body   => $helps[1]\n";
+	print "  -H --header => $helps[2]\n";
+	exit 1;
 }
+
+exit 0;
+
+#
+# Local variables:
+# tab-width: 4
+# c-basic-offset: 4
+# End:
+# vim600: noet sw=4 ts=4 fdm=marker
+# vim<600: noet sw=4 ts=4
+#
