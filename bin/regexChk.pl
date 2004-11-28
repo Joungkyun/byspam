7e00fe1d35af1424afed588e304b06094a8dd192
JoungKyun Kim <joungkyun@daum.net>
2004-11-28 11:50:11 +0000

    added use strict

--- bin/regexChk.pl
+++ bin/regexChk.pl
@@ -1,21 +1,27 @@
-#!@PERLPATH@ -W
+#!/usr/bin/perl -W
 #
 # Perl regualr expression check utility in BySPAM
 # JoungKyun Kim <http://www.oops.org>
-# $Id: regexChk.pl,v 1.2 2004-11-27 19:06:48 oops Exp $
+# $Id: regexChk.pl,v 1.3 2004-11-28 11:50:11 oops Exp $
 #
-my $lcn;
-my $lc;
+
+use strict;
+
+my $charset;
 my $USAGES;
 my $targetA;
 my $targetB;
+my $WN;
 
-$lcn = $ENV{"LANG"};
-$lcn =~ s/^ko.*/ko/;
-if ($lcn eq "ko") { $lc = 1; }
+$charset = $ENV{"LANG"};
+CHARSET: {
+	( $charset =~ m/^ko/i ) and $charset = "EUC-KR", last CHARSET;
+	( $charset =~ m/^utf/i ) and $charset = "UTF-8", last CHARSET;
+	$charset = "";
+}
 
-if ( $#ARGV < 1 ) {
-  if($lc) {
+if ( $#ARGV != 1 ) {
+  if( $charset eq "EUC-KR" ) {
     $USAGES  = "사용법";
     $targetA = "체크 할 문자열";
     $targetB = "체크 할 정규표현식 패턴";
@@ -40,10 +46,21 @@ my $pattern;
 $string = "$ARGV[0]";
 $pattern = "$ARGV[1]";
 
-if($string =~ /$pattern/igo) {
-  if($lc) { print "사용할 수 있는 표현식 입니다.\n"; }
+if( $string =~ /$pattern/ig ) {
+  if ($charset eq "EUC-KR" ) { print "사용할 수 있는 표현식 입니다.\n"; }
   else { print "This pattern is right\n" }
 } else {
-  if($lc) { print "표현식에 문제가 있습니다.\n"; }
-  else { print "This pattern is wrong\n"; }
+  if ( $charset eq "EUC-KR" ) { print "표현식이 주어진 문자열과 매치가 되지 않습니다.\n"; }
+  else { print "This expression is not match given string\n"; }
 }
+
+exit 0;
+
+#
+# Local variables:
+# tab-width: 4
+# c-basic-offset: 4
+# End:
+# vim600: noet sw=4 ts=4 fdm=marker
+# vim<600: noet sw=4 ts=4
+#
