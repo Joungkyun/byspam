1762922a205e680a3cbc5c4d4f1e3860e8a3c9be
JoungKyun Kim <joungkyun@daum.net>
2004-11-30 11:43:05 +0000

    chaged command help format

--- include/Trash.pm
+++ include/Trash.pm
@@ -3,7 +3,7 @@
 #
 # scripted by JoungKyun Kim <http://www.oops.org>
 #
-# $Id: Trash.pm,v 1.2 2004-11-30 11:38:03 oops Exp $
+# $Id: Trash.pm,v 1.3 2004-11-30 11:43:05 oops Exp $
 #
 
 package Byspam::Trash;
@@ -256,7 +256,7 @@ sub printTrash {
 		system ("clear");
 		print "=============================================================================\n";
 		print "[1;37mby SPAM Trash Viewer[7;0m $main::version by JoungKyun Kim <http://www.oops.org>\n";
-		print "Command [ q - quit | no - view no | d no - del no | enter - next | b - back ]\n";
+		print "Command [ [1;37mq[7;0muit | [1;37mn[7;0mumber | [1;37md[7;0melete | [1;37mr[7;0mecovery | enter - next | b - back ]\n";
 		print "=============================================================================\n";
 		print "Current Date: $filedates\n";
 		print "$p_page page / total $lastpage pages\n\n";
