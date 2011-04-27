b8da87036519a4b2d68f64b059f182434ed1bce5
JoungKyun Kim <joungkyun@daum.net>
2011-04-27 07:43:27 +0000

    rename tag name

---
+++ bin/regexChk.pl
@@ -0,0 +1,44 @@
+#!@PERLPATH@ -W
+my $lcn;
+my $lc;
+my $USAGES;
+my $targetA;
+my $targetB;
+
+$lcn = $ENV{"LANG"};
+$lcn =~ s/^ko.*/ko/;
+if ($lcn eq "ko") { $lc = 1; }
+
+if ( $#ARGV < 1 ) {
+  if($lc) {
+    $USAGES  = "사용법";
+    $targetA = "체크 할 문자열";
+    $targetB = "체크 할 정규표현식 패턴";
+    $WN      = "A 와 B 는 꼭 작음 따옴표로 감싸야 한다."
+  } else {
+    $USAGES  = "USAGE";
+    $targetA = "Check string";
+    $targetB = "Check regex pattern";
+    $WN      = "A and B is must quoted single quote"
+  }
+
+  print "$USAGES : regexChk.pl 'A' 'B'\n";
+  print "          A => $targetA\n";
+  print "          B => $targetB\n";
+  print "          $WN\n";
+  exit;
+}
+
+my $string;
+my $pattern;
+
+$string = "$ARGV[0]";
+$pattern = "$ARGV[1]";
+
+if($string =~ /$pattern/igo) {
+  if($lc) { print "사용할 수 있는 표현식 입니다.\n"; }
+  else { print "This pattern is right\n" }
+} else {
+  if($lc) { print "표현식에 문제가 있습니다.\n"; }
+  else { print "This pattern is wrong\n"; }
+}
